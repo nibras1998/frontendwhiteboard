6d79a1bb3f7e9ce7071d145bf3d866a43e549a5c
Muhammed Nibras <mohdnibras1998@gmail.com>
2022-12-25 16:16:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+
+import { fabric } from "fabric";
+import {useEffect,useState} from 'react'
 import './App.css';
 
 function App() {
+  useEffect(()=>{
+    const canvas = new fabric.Canvas('canvas');
+    canvas.freeDrawingBrush.color="red"
+    canvas.isDrawingMode = true
+  },[])
+  const clicked=()=>{
+    let eraser = new fabric.PatternBrush("canvas");
+    eraser.add(new fabric.Circle(
+      { radius: 30, 
+        fill: 'white', 
+        top: 100, 
+        left: 100 }));
+    eraser.freeDrawingBrush.color="white"
+    eraser.freeDrawingBrush.width = "6"
+    
+  }
+  const eraseAll=()=>{
+    const all = new fabric.Canvas('canvas');
+    all.remove()
+  }
+  
+const Rectangle=()=>{
+const canvas = new fabric.Canvas('canvas');
+var rect = new fabric.Rect();
+rect.set({ width: 100, 
+  height: 200, 
+  fill: 'white', 
+  });
+}
+
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+      <canvas id="canvas"/>
+      </div>
+      <div id="btn">
+        <button onClick={clicked} id="erase">Erase</button>
+        <button onClick={eraseAll} id="eraseall">Erase All</button>
+      </div>
+     
     </div>
   );
 }
